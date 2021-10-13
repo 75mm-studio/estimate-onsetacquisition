3b53c8f06516ae645931e92bd5f71c8c7ec3f333
hanwoong kim <hello@lazypic.org>
2021-10-13 10:16:30 +0900

    리펙토링

--- publish.sh
+++ publish.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-aws s3 cp ./onset-pricing-eng.html s3://75mm.studio --acl public-read --profile 75mm.studio
-aws s3 cp ./onset-pricing-kor.html s3://75mm.studio --acl public-read --profile 75mm.studio
-aws s3 cp ./js/onset-pricing-eng.js s3://75mm.studio/js/ --acl public-read --profile 75mm.studio
-aws s3 cp ./js/onset-pricing-kor.js s3://75mm.studio/js/ --acl public-read --profile 75mm.studio
-aws s3 cp ./css/onset-pricing.css s3://75mm.studio/css/ --acl public-read --profile 75mm.studio
-aws s3 cp ./img/onset-pricingicon.png s3://75mm.studio/img/ --acl public-read --profile 75mm.studio
+aws s3 cp ./estimate-3dscan-eng.html s3://estimates.75mm.studio --acl public-read --profile 75mm.studio
+aws s3 cp ./estimate-3dscan-kor.html s3://estimates.75mm.studio --acl public-read --profile 75mm.studio
+aws s3 cp ./js/estimate-3dscan-eng.js s3://estimates.75mm.studio/js/ --acl public-read --profile 75mm.studio
+aws s3 cp ./js/estimate-3dscan-kor.js s3://estimates.75mm.studio/js/ --acl public-read --profile 75mm.studio
+aws s3 cp ./css/estimate-3dscan.css s3://estimates.75mm.studio/css/ --acl public-read --profile 75mm.studio
+aws s3 cp ./img/estimate-3dscan.png s3://estimates.75mm.studio/img/ --acl public-read --profile 75mm.studio
+aws s3 cp ./img/logo_v02.svg s3://estimates.75mm.studio/img/ --acl public-read --profile 75mm.studio

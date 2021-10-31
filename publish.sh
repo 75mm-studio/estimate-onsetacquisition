3e7d93af45a004164725d0d36f76a24d61bf89ce
hanwoong kim <hello@lazypic.org>
2021-10-31 21:11:32 +0900

    가격정책 반영

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-aws s3 cp ./estimate-3dscan-eng.html s3://estimates.75mm.studio --acl public-read --profile 75mm.studio
-aws s3 cp ./estimate-3dscan-kor.html s3://estimates.75mm.studio --acl public-read --profile 75mm.studio
-aws s3 cp ./js/estimate-3dscan-eng.js s3://estimates.75mm.studio/js/ --acl public-read --profile 75mm.studio
-aws s3 cp ./js/estimate-3dscan-kor.js s3://estimates.75mm.studio/js/ --acl public-read --profile 75mm.studio
-aws s3 cp ./css/estimate-3dscan.css s3://estimates.75mm.studio/css/ --acl public-read --profile 75mm.studio
-aws s3 cp ./img/estimate-3dscan.png s3://estimates.75mm.studio/img/ --acl public-read --profile 75mm.studio
+aws s3 cp ./estimate-onsetacquisition-eng.html s3://estimates.75mm.studio --acl public-read --profile 75mm.studio
+aws s3 cp ./estimate-onsetacquisition-kor.html s3://estimates.75mm.studio --acl public-read --profile 75mm.studio
+aws s3 cp ./js/estimate-onsetacquisition-eng.js s3://estimates.75mm.studio/js/ --acl public-read --profile 75mm.studio
+aws s3 cp ./js/estimate-onsetacquisition-kor.js s3://estimates.75mm.studio/js/ --acl public-read --profile 75mm.studio
+aws s3 cp ./css/estimate-onsetacquisition.css s3://estimates.75mm.studio/css/ --acl public-read --profile 75mm.studio
+aws s3 cp ./img/estimate-onsetacquisition.png s3://estimates.75mm.studio/img/ --acl public-read --profile 75mm.studio
 aws s3 cp ./img/logo_v02.svg s3://estimates.75mm.studio/img/ --acl public-read --profile 75mm.studio
